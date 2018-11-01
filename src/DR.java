8a220e16a40bedf2a4e41956b2cf9126d18f794c
Hafez Bahrasemani <hafez.bahrasemany@gmail.com>
2018-11-01 01:00:54 -0700

    creating a class Register and extending all registers from it

--- src/DR.java
+++ src/DR.java
@@ -1,2 +1,18 @@
+/**
+ * DR class represents a Data Register that is a 16bits register
+ * which holds some data
+ *
+ * @author hafez
+ */
 public class DR {
+    int[] drRegister;
+
+    /**
+     * Constructor
+     */
+    public DR() {
+        this.drRegister = new int[16];
+    }
+
+    
 }
--- src/PC.java
+++ src/PC.java
@@ -1,5 +1,56 @@
 /**
- * 
+ * the PC class represents the Program Counter register of a mano computer
+ *
+ * This is a 12bits register
+ *
+ * @author hafez
  */
 public class PC {
+    int[] pcRegister;
+
+    /**
+     * Constructor of the AR class
+     */
+    public PC() {
+        pcRegister = new int[12];
+    }
+
+
+    /**
+     * read
+     *
+     * This method reads the address stored in PC
+     * @return the array containing address
+     */
+    public int[] read(){
+        int[] content = new int[12];
+
+        for(int i = 0; i < 12; i++){
+            content[i] = pcRegister[i];
+        }
+
+        return content;
+    }
+
+
+    /**
+     * write
+     * @param data the array to be write in PC
+     */
+    public void write(int[] data){
+        for(int i = 0; i < 12;i++){
+            pcRegister[i] = data[i];
+        }
+    }
+
+    @Override
+    public String toString() {
+        String str = null;
+
+        for (int i = 0; i < 12; i++){
+            str += pcRegister[11 - i];
+        }
+
+        return "PC: " + str;
+    }
 }
